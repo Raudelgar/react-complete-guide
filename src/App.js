027d6736c64e60f05ceed6a637ecb4ab5ccb8db3
Raudel Garcia <raudel24@gmail.com>
2018-03-20 11:46:02 -0400

    Adding Person Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
+      //jsx syntax
       <div className="App">
         <h1>Hi, I am a React App</h1>
+        <p>This is working!!</p>
+        <Person/>
       </div>
     );
+
+    //this is the same as above
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this works?'));
   }
 }
 
