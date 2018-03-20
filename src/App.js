4d7757131ad7fe17295e22f48105402b56e31174
Raudel Garcia <raudel24@gmail.com>
2018-03-20 15:42:36 -0400

    Adding onClick event handler

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    person: [
+      {name: "Raudel", gender: "he", age: 31},
+      {name: "Marjorie", gender: "she",age: 30}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was cliked!!");
+  }
+
   render() {
     return (
       //jsx syntax
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is working!!</p>
-        <Person name="Raudel" gender="he" age="31"/>
-        <Person name="Marjorie" gender="she" age="30">My hobbies: Teaching</Person>
+        <button onClick={this.switchNameHandler}>Switch Names</button>
+        <Person name={this.state.person[0].name} gender={this.state.person[0].gender} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} gender={this.state.person[1].gender} age={this.state.person[1].age}>My hobbies: Teaching</Person>
       </div>
     );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,9 +6,7 @@ const person = (props) => {
       <p>This is {props.name}, and {props.gender} is {props.age} years old</p>
       <p>{props.children}</p>    
     </div>
-  ); 
-
-  <p>This is {props.name}, and {props.gender} is {props.age} years old</p>
+  )
 };
 
 export default person;
