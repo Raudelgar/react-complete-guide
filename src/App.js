5bb6bcad7ac4750b81183ee237b2e8334b395a94
Raudel Garcia <raudel24@gmail.com>
2018-03-23 13:07:15 -0400

    Adding Radium for sudo selectors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -73,7 +74,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -94,6 +99,10 @@ class App extends Component {
           </div>
       );
       designBtn.backgroundColor = "red";
+      designBtn[":hover"]= {
+        backgroundColor: "lightblue",
+        color: "black"
+      }
     }
 
     let classes = []; 
@@ -122,4 +131,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
